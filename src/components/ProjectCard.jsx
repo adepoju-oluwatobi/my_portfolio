86d43ad82ece9da1903f9b738b5061bb1c59d6f0
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-09-27 12:38:10 +0100

    added animations

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,7 +3,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Header from "./Header";
 import Github from "../assets/github.png";
 import Linkedin from "../assets/linkedin-logo.png";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence, useAnimation } from "framer-motion";
 
 const linkedinLink = "https://www.linkedin.com/in/oluwatobi-adepoju-4a242898/";
 const githubLink = "https://github.com/adepoju-oluwatobi";
@@ -11,7 +11,9 @@ const githubLink = "https://github.com/adepoju-oluwatobi";
 const projects = [
   {
     name: "Movie Box",
-    description: "An internship project from HNG. API fetched from TMDB.",
+    tag: "Entertainment",
+    description: `A web App to search for movies. It displays the year it was made,
+    the genre, the maker of the movie and the ability to see a thriler about the movie`,
     image: "/movie_app.png",
     vite: "/vite.svg",
     react: "/react.svg",
@@ -27,8 +29,10 @@ const projects = [
   },
   {
     name: "Fashion Store",
+    tag: "Ecommerce",
     description: `An Ecommerce website developed to sharpen my 
-      skill on API fetching and ecommerce development.`,
+      skill on API fetching and ecommerce development. It displays 
+      some fashion items`,
     image: "/shoe_store.png",
     link: "https://shoe-store-livid.vercel.app/",
     vite: "/vite.svg",
@@ -37,7 +41,8 @@ const projects = [
   },
   {
     name: "Intro Section",
-    description: "A challenge by frontend mentor",
+    description: `A challenge by frontend mentor. It was created using Vite + React
+    and tailwind css. This project test my skills on positioning and responsivity.`,
     image: "/intro_section.png",
     link: "https://intro-section-with-dropdown-navigation-ruby.vercel.app/",
     vite: "/vite.svg",
@@ -56,7 +61,8 @@ const projects = [
   },
   {
     name: "Patricia Clone",
-    description: "A Clone of the patricia website",
+    description: `A Clone of the original patricia website. This project was 
+    made with react and tailwind css. It test my skill on positioning and mordern design`,
     image: "/patricia.png",
     link: "https://patricia-clone.vercel.app/",
     vite: "/vite.svg",
@@ -66,7 +72,8 @@ const projects = [
   {
     name: "Rating Component",
     description:
-      "A challenge by frontend mentor to build  component for rating",
+      `A challenge by frontend mentor to build  component for rating. I used 
+      useState to achive the solution. Users can select a ratings for a product.`,
     image: "/ratings.png",
     link: "https://06-interactive-rating-component.vercel.app/",
     vite: "/vite.svg",
@@ -96,6 +103,13 @@ const projects = [
 ];
 function ProjectCard() {
   const [activeIndex, setActiveIndex] = useState(0);
+  const controls = useAnimation();
+
+  useEffect(() => {
+    controls.start({ x: 0 }); // Animate the first card
+  }, [controls]);
+  
+
 
   const backgroundGradient = {
     background: `radial-gradient(circle at center top, #292929 0%, #101010 50%)`,
@@ -137,7 +151,7 @@ function ProjectCard() {
     hover: {
       scale: isMobile ? 1 : 1.1, // Apply the hover effect only on non-mobile devices
       transition: {
-        duration: 0.3,
+        duration: 0.1,
         ease: "easeInOut",
       },
     },
@@ -146,14 +160,27 @@ function ProjectCard() {
   return (
     <div style={{ ...backgroundGradient, overflow: "hidden" }}>
       <Header />
-      <div className="text-white mt-[25%] md:mt-[5%] grid grid-cols-1 md:grid-cols-4 py-2">
+      <motion.div
+  className="text-white mt-[25%] md:mt-[5%] grid grid-cols-1 md:grid-cols-4 py-2"
+  initial="hidden"
+  animate="visible"
+  variants={{
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { staggerChildren: 0.2 } }, // Adjust the stagger delay as needed
+  }}
+  controls={controls}
+>
         {projects.map((project, index) => (
-          <motion.div
-          key={index}
-          className="py-4 mb-4"
-          whileHover={{ scale: 1.1 }} // Enlarge the card by 10% on hover
-        >
-            <div className="w-[300px] md:w-[400px] md:h-[600px] m-auto bg-[#232323] rounded-[30px]">
+         <a href={project.link} target="_blank" key={index}>
+         <motion.div
+           className="py-4 mb-4"
+           variants={{
+             hidden: { x: -1000 }, // Move the card to the left of the screen
+             visible: { x: 0 }, // Bring the card back to its original position
+           }}
+           whileHover={{ scale: 1.1 }} // Enlarge the card by 10% on hover
+         >
+            <div className="w-[300px] md:w-[400px] md:h-[600px] m-auto bg-[#181818] shadow-2xl rounded-[30px]">
               <img src={project.image} alt="" />
               <div className="px-8 py-6">
                 <p style={fontFamily} className="text-xl">
@@ -169,7 +196,7 @@ function ProjectCard() {
                     alt=""
                   />
                  <a href={project.link} target="_blank">
-                 <button className="text-white text-xs px-6 py-2 rounded bg-[#303030] font-bold shadow-lg md:w-fit">
+                 <button className="text-white text-xs px-6 py-2 rounded bg-[#303030] shadow-2xl font-bold md:w-fit">
                     View site
                   </button>
                  </a>
@@ -180,8 +207,9 @@ function ProjectCard() {
               </div>
             </div>
           </motion.div>
+         </a>
         ))}
-      </div>
+     {/**end */} </motion.div>
 
       <section className="flex md:flex-col gap-5 mt-[5%] md:gap-8 md:mt-[-1%] ml-10 mb-4">
         <motion.a
